fc1cf2d52921b1092254bd2cc558c8e1a14792c2
Julian Aloise <aloisejulian@gmail.com>
2022-06-21 10:43:32 -0300

    email text

---
+++ templates/email/text/civil.php
@@ -0,0 +1,30 @@
+Buenos días.
+Dicha información también será notificada por teléfono, se lo agregara a un  grupo de  WhatsApp con el resto de los aspirantes
+que se presentarán el mismo día. El objetivo del grupo es para poder evacuar cualquier duda sobre la preparación de los estudios que
+se mencionan en este mail. Una vez que se presenten al día del examen, se corrobora el presentismo para comunicar a Incorporaciones de Policía de la ciudad y el grupo se disolverá.
+
+IMPORTANTE LUEGO DE LEER ESTE CORREO, ES INDISPENSABLE LA CONFIRMACIÓN DE ASISTENCIA DE LO QUE SE LE ESTA NOTIFICANDO RESPONDIEDO ESTE CORREO.
+
+Se le comunica que el día <?= $date ?>. tiene turno asignado para la realización de examen Preocupacional para el área de incorporaciones de Policía de la Ciudad.
+
+PERSONAL CIVIL ADMINISTRATIVO.
+
+Por favor se ruega puntualidad ya que es por orden de llegada y así  evitar atrasos en el cronograma de los estudios a realizar.
+
+Dirección: Centro Médico Nogoya, sito La Rioja N° 682 entre las calles México y Venezuela, Balvanera CABA.
+
+
+Importante: Para la correcta realización de los estudios requeridos deberá de tener en cuenta:
+
+
+1) Traer DNI. (en caso de pérdida o trámite, presentar fotocopia de partida de nacimiento y app Mi Argentina)
+2) 12 hs. de ayuno previo y retención de 3 hs. de orina. (La recolección  de la muestra es realizada en el centro médico donde le suministrarán un recipiente estéril)
+3) Reposo auditivo previo de 48 hs. (no utilizar auriculares).
+4) Ropa interior deportiva en mujeres. (para la realización de RX)
+5) En el caso de las mujeres si están en su periodo menstrual, utilizar tampón a la hora de realizar el examen de orina.
+6) El día del estudio no concurrir con aros y piercing.
+7) En caso de utilizar lentes con algún tipo de graduación, por favor presentarse con ellos para poder realizar el examen ocular correctamente.
+
+
+
+Desde ya muchas gracias.
